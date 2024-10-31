34e3e3763d66d3e7a56b0cfabc342dbd6f748143
ASUS <ASUS@DEVAN-W>
2024-10-31 16:24:46 +0700

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        public string name;
+        public double balance;
+
+        public BankAccount(string name, double initialBalance)
+        {
+            this.name = name;
+            balance = initialBalance;
+        }
+
+        public string Debit(double amount)
+        {
+            if (amount < 0)
+            {
+                return "Error: Debit amount cannot be negative."; 
+            }
+
+            if (amount > balance)
+            {
+                return "Error: Insufficient funds for this transaction.";
+            }
+
+            balance -= amount;
+            return "Transaction successful. Amount debited: " + amount;
+        }
+    }
+}
